029f8193c7b66f112705f1d772606eb6f6771218
Umar Farooq <iumrfarooq@gmail.com>
2024-08-13 18:50:41 +0500

    feat: Refactor TraderjoesSpider to improve code readability and maintainability

--- scrapy_store_scrapers/spiders/traderjoes.py
+++ scrapy_store_scrapers/spiders/traderjoes.py
@@ -1,19 +1,20 @@
+import re
+import json5 as json
+from typing import Generator, Dict, Any
 from datetime import datetime
-import re 
-import json
-from typing import Generator
 
 import scrapy
-import scrapy.http
 from scrapy.exceptions import DropItem
 from scrapy.loader import ItemLoader
-from scrapy_store_scrapers.items import TraderjoesStoreItem
+from scrapy.http import Response
 from itemloaders.processors import TakeFirst, MapCompose
+
+from scrapy_store_scrapers.items import TraderjoesStoreItem
 
 class TraderjoesSpider(scrapy.Spider):
     name = "traderjoes"
     allowed_domains = ["locations.traderjoes.com"]
-    start_urls = ["http://locations.traderjoes.com/"]
+    start_urls = ["https://locations.traderjoes.com/"]
 
     # Constants
     LINKS_XPATH = '//div[@class="itemlist"]/a/@href'
@@ -21,21 +22,23 @@ class TraderjoesSpider(scrapy.Spider):
     JSON_SCRIPT_XPATH = "//script[@type='application/ld+json' and contains(text(),'GroceryStore')]/text()"
     NAME_XPATH = "//h1/text()"
 
-    def parse(self, response: scrapy.http.Response) -> Generator[scrapy.Request, None, None]:
+    def parse(self, response: Response) -> Generator[scrapy.Request, None, None]:
         """Recursively follow links on the main page until store pages are reached."""
-        
-        if response.xpath(self.LINKS_XPATH):
-            for link in response.xpath(self.LINKS_XPATH).getall():
-                yield response.follow(link, callback=self.parse)
-                break
-        elif response.xpath(self.STORE_LINKS_XPATH):
-            for link in response.xpath(self.STORE_LINKS_XPATH).getall():
-                yield response.follow(link, callback=self.parse_store)
-                break
+
+        links = response.xpath(self.LINKS_XPATH).getall()
+        store_links = response.xpath(self.STORE_LINKS_XPATH).getall()
+        if links:
+            yield from self.follow_links(response, links, self.parse)
+        elif store_links:
+            yield from self.follow_links(response, store_links, self.parse_store)
         else:
             self.logger.warning(f"No links found on {response.url}")
-    
-    def parse_store(self, response: scrapy.http.Response) -> TraderjoesStoreItem:
+
+    def follow_links(self, response: Response, links: list, callback) -> Generator[scrapy.Request, None, None]:
+        for link in links:
+            yield response.follow(link, callback=callback)
+
+    def parse_store(self, response: Response) -> TraderjoesStoreItem:
         loader = ItemLoader(item=TraderjoesStoreItem(), response=response)
         loader.default_output_processor = TakeFirst()
 
@@ -44,64 +47,64 @@ class TraderjoesSpider(scrapy.Spider):
         if not script_text:
             raise DropItem(f"No script text found on {response.url}")
         
-        script_text = self.remove_comments(script_text)
         
-        store_raw_dict = json.loads(script_text)
+        try:
+            store_raw_dict = json.loads(script_text)
+        except Exception as e:
+            raise DropItem(f"Invalid JSON on {response.url}")
 
-        loader.add_xpath('name', self.NAME_XPATH, MapCompose(self.clean_store_name))
-        loader.add_value('number', int(store_raw_dict.get("@id")))
-
-        address_dict = store_raw_dict.get("address")
-
-        address = f'{address_dict.get("streetAddress")}, {address_dict.get("addressLocality")}, {address_dict.get("addressRegion")} {address_dict.get("postalCode")}'
-
-        loader.add_value('address', address)
-        loader.add_value('phone_number', store_raw_dict.get("telephone"))
-
-        geo_info = store_raw_dict.get("geo")
-        loader.add_value('location', {
-            'type': 'Point',
-            'coordinates': [geo_info.get("longitude"), geo_info.get("latitude")]
-        })
-
-        hours_dict = {}
-        hours_raw_list = store_raw_dict.get("openingHoursSpecification")
-
-        for day in hours_raw_list:
-            day_name = day.get("dayOfWeek")[0]
-            hours_dict[day_name.lower()] = {
-                'open': self.convert_to_12_hour(day.get("opens")),
-                'close': self.convert_to_12_hour(day.get("closes"))
-            }
-        
-        loader.add_value('hours', hours_dict)
+        self.populate_item(loader, store_raw_dict)
         return loader.load_item()
 
+    def populate_item(self, loader: ItemLoader, store_raw_dict: Dict[str, Any]) -> None:
+        loader.add_xpath('name', self.NAME_XPATH, MapCompose(self.clean_store_name))
+        loader.add_value('number', int(store_raw_dict.get("@id", 0)))
+
+        address_dict = store_raw_dict.get("address", {})
+        address = self.format_address(address_dict)
+        loader.add_value('address', address)
+
+        loader.add_value('phone_number', store_raw_dict.get("telephone"))
+
+        geo_info = store_raw_dict.get("geo", {})
+        loader.add_value('location', self.format_location(geo_info))
+
+        hours_raw_list = store_raw_dict.get("openingHoursSpecification", [])
+        hours_dict = self.format_hours(hours_raw_list)
+        loader.add_value('hours', hours_dict)
+
     @staticmethod
-    def remove_comments(json_text):
-        # Remove single-line comments
-        json_text = re.sub(r'(?<!:)//.*', '', json_text)
-        
-        # Remove multi-line comments
-        json_text = re.sub(r'/\*[\s\S]*?\*/', '', json_text)
-        
-        # Remove any trailing commas
-        json_text = re.sub(r',\s*}', '}', json_text)
-        json_text = re.sub(r',\s*]', ']', json_text)
-        
-        return json_text
+    def format_address(address_dict: Dict[str, str]) -> str:
+        return f"{address_dict.get('streetAddress', '')}, {address_dict.get('addressLocality', '')}, {address_dict.get('addressRegion', '')} {address_dict.get('postalCode', '')}"
+
+    @staticmethod
+    def format_location(geo_info: Dict[str, float]) -> Dict[str, Any]:
+        return {
+            'type': 'Point',
+            'coordinates': [geo_info.get("longitude", 0), geo_info.get("latitude", 0)]
+        }
+
+    def format_hours(self, hours_raw_list: list) -> Dict[str, Dict[str, str]]:
+        return {
+            day.get("dayOfWeek")[0].lower(): {
+                'open': self.convert_to_12_hour(day.get("opens", "")),
+                'close': self.convert_to_12_hour(day.get("closes", ""))
+            }
+            for day in hours_raw_list
+        }
 
     @staticmethod
     def convert_to_12_hour(time_str: str) -> str:
-        """Convert 24-hour time string to 12-hour format."""
-        time_obj = datetime.strptime(time_str, '%H:%M')
-        return time_obj.strftime('%I:%M %p').lower()
+        try:
+            time_obj = datetime.strptime(time_str, '%H:%M')
+            return time_obj.strftime('%I:%M %p').lower().lstrip('0')
+        except ValueError:
+            return ""
 
     @staticmethod
-    def clean_store_name(name):
+    def clean_store_name(name: str) -> str:
         return re.sub(r'\s*\(\d+\)\s*$', '', name)
     
     @staticmethod
     def clean_text(text: str) -> str:
-        """Clean and strip whitespace from text"""
         return text.strip() if text else ""
