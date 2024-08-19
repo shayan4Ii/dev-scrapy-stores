3bd6b3b013f8d4237cb6424f8b2e16e7358a79c5
Umar Farooq <iumrfarooq@gmail.com>
2024-08-19 18:18:34 +0500

    chore: Update default headers for MeijerSpider

--- scrapy_store_scrapers/spiders/meijer.py
+++ scrapy_store_scrapers/spiders/meijer.py
@@ -72,16 +72,19 @@ class MeijerSpider(scrapy.Spider):
     def get_default_headers(self) -> Dict[str, str]:
         """Return default headers for requests."""
         return {
-            "accept": "application/json, text/plain, */*",
-            "accept-encoding": "gzip, deflate, br",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-encoding": "gzip, deflate, br, zstd",
             "accept-language": "en-US,en;q=0.9",
-            "connection": "keep-alive",
+            "cache-control": "max-age=0",
             "dnt": "1",
-            "sec-ch-ua": '"Chromium";v="112", "Google Chrome";v="112"',
+            "priority": "u=0, i",
+            "sec-ch-ua": '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
             "sec-ch-ua-mobile": "?0",
             "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
         }
