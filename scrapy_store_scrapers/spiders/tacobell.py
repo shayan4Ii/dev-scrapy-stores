6d3f7000b7868432b821770d484b07ab8aee8bdc
Umar Farooq <iumrfarooq@gmail.com>
2024-09-03 09:28:34 +0500

    Seperated the spider and usage of scrapy items

--- scrapy_store_scrapers/spiders/tacobell.py
+++ scrapy_store_scrapers/spiders/tacobell.py
@@ -1,6 +1,6 @@
 import re
 import json
-from typing import Generator, Any, Union
+from typing import Generator, Any, Union, Dict
 
 import scrapy
 from scrapy.http import Response
@@ -8,36 +8,24 @@ from scrapy_store_scrapers.items import ZipcodeLongLatItem
 from scrapy.loader import ItemLoader
 from itemloaders.processors import TakeFirst, MapCompose
 
-class TacobellSpider(scrapy.Spider):
-    name = "tacobell"
-    allowed_domains = ["www.tacobell.com"]
+import json
+import scrapy
+
+class TacobellZipcodeSpider(scrapy.Spider):
+    name = "tacobell_zipcode_spider"
+    allowed_domains = ["api.tacobell.com"]
 
     custom_settings = {
         'FEEDS': {
-            'tacobell_zipcode_data.json': {
+            'zipcode_data.json': {
                 'format': 'json',
-                'item_classes': [ZipcodeLongLatItem]
-            },
-            'tacobell_data.json': {
-                'format': 'json',
-                'item_classes': [dict]
             }
-        },
-        'ITEM_PIPELINES': {
-            'scrapy_store_scrapers.pipelines.TacobellDuplicatesPipeline': 300,
         }
     }
 
     LOCATION_API_URL_FORMAT = "https://api.tacobell.com/location/v1/{}"
-    STORES_API_URL = "https://www.tacobell.com/tacobellwebservices/v4/tacobell/stores?latitude={}&longitude={}"
 
-    def start_requests(self) -> Generator[scrapy.Request, None, None]:
-        """
-        Load zipcodes from the JSON file and generate requests for each zipcode.
-
-        Returns:
-            Generator[scrapy.Request, None, None]: A generator yielding scrapy.Request objects.
-        """
+    def start_requests(self):
         with open('zipcodes.json', 'r') as f:
             zipcodes_data = json.load(f)
 
@@ -46,44 +34,46 @@ class TacobellSpider(scrapy.Spider):
                 url = self.LOCATION_API_URL_FORMAT.format(zipcode)
                 yield scrapy.Request(url, self.parse_zipcode_info, cb_kwargs={'zipcode': zipcode})
 
-    def parse_zipcode_info(self, response: Response, zipcode: str) -> Generator[Union[scrapy.Request, ZipcodeLongLatItem], None, None]:
-        """
-        Parse the zipcode location JSON and yield the location data. Also generate a request for the stores data.
-
-        Args:
-            response (Response): The response object containing the zipcode location JSON.
-            zipcode (str): The zipcode being processed.
-
-        Yields:
-            Union[scrapy.Request, ZipcodeLongLatItem]: Either a new request for store data or a ZipcodeLongLatItem.
-        """
+    def parse_zipcode_info(self, response, zipcode):
         try:
             zipcode_data = response.json()["geometry"]
         except Exception as e:
             self.logger.error(f"Error parsing zipcode location JSON from {response.url}: {e}")
             return
 
-        yield scrapy.Request(self.STORES_API_URL.format(zipcode_data['lat'], zipcode_data['lng']), self.parse_stores)
+        yield {
+            'zipcode': zipcode,
+            'latitude': zipcode_data['lat'],
+            'longitude': zipcode_data['lng']
+        }
 
-        loader = ItemLoader(item=ZipcodeLongLatItem(), response=response)
-        loader.default_output_processor = TakeFirst()
+class TacobellStoreSpider(scrapy.Spider):
+    name = "tacobell_store_spider"
+    allowed_domains = ["www.tacobell.com"]
 
-        loader.add_value('zipcode', zipcode)
-        loader.add_value('latitude', zipcode_data['lat'])
-        loader.add_value('longitude', zipcode_data['lng'])
+    custom_settings = {
+        'FEEDS': {
+            'store_data.json': {
+                'format': 'json',
+            }
+        },
+        'ITEM_PIPELINES': {
+            'scrapy_store_scrapers.pipelines.TacobellDuplicatesPipeline': 300,
+        }
+    }
 
-        yield loader.load_item()
+    STORES_API_URL = "https://www.tacobell.com/tacobellwebservices/v4/tacobell/stores?latitude={}&longitude={}"
 
-    def parse_stores(self, response: Response) -> Generator[dict[str, Any], None, None]:
-        """
-        Parse the stores JSON and yield the store data.
+    def start_requests(self) -> Generator[scrapy.Request, None, None]:
+        with open(r'data\tacobell_zipcode_data.json', 'r') as f:
+            zipcode_data = json.load(f)
 
-        Args:
-            response (Response): The response object containing the stores JSON.
+        for entry in zipcode_data:
+            url = self.STORES_API_URL.format(entry['latitude'], entry['longitude'])
+            yield scrapy.Request(url, self.parse_stores)
+            break
 
-        Yields:
-            Dict[str, Any]: Store data dictionaries.
-        """
+    def parse_stores(self, response: Response) -> Generator[Dict[str, Any], None, None]:
         try:
             stores_data = response.json()['nearByStores']
         except Exception as e:
@@ -104,8 +94,9 @@ class TacobellSpider(scrapy.Spider):
 
     @staticmethod
     def format_time(time_str: str) -> str:
-        """Add a space before 'am' or 'pm' if not present."""
-        return re.sub(r'(\d+)([ap]m)', r'\1 \2', time_str)
+        if isinstance(time_str, str):
+            return re.sub(r'(\d+)([ap]m)', r'\1 \2', time_str)
+        return time_str
 
     def _get_hours(self, opening_hours: dict) -> dict:
         hours_info = {}
@@ -123,7 +114,6 @@ class TacobellSpider(scrapy.Spider):
         return hours_info
 
     def _get_address(self, address_info: dict) -> str:
-        """Get the formatted store address."""
         try:
             address_parts = [
                 address_info.get("line1", ""),
@@ -143,7 +133,6 @@ class TacobellSpider(scrapy.Spider):
             return ""
 
     def _get_location(self, loc_info: dict) -> dict:
-        """Get the store location in GeoJSON Point format."""
         try:
             latitude = loc_info.get('latitude')
             longitude = loc_info.get('longitude')
