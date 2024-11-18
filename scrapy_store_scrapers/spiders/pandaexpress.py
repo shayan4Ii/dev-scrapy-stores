00267a299827c80b1b6f5ac66bbf7ff3cfa15d44
Umar Farooq <iumrfarooq@gmail.com>
2024-11-18 23:35:39 +0500

    fix: refactor Pandaexpress spider to improve data extraction and error handling for location and hours

--- scrapy_store_scrapers/spiders/pandaexpress.py
+++ scrapy_store_scrapers/spiders/pandaexpress.py
@@ -1,104 +1,108 @@
+import json
+from typing import Any
 import scrapy
-from scrapy_store_scrapers.utils import *
 
 
-
-
-class PandaExpress(scrapy.Spider):
+class PandaexpressSpider(scrapy.Spider):
     name = "pandaexpress"
+    allowed_domains = ["www.pandaexpress.com"]
+    start_urls = ["https://www.pandaexpress.com/locations"]
+
     custom_settings = {
-        "DOWNLOAD_HANDLERS": {
-            "https": "scrapy_impersonate.ImpersonateDownloadHandler",
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'COOKIES_ENABLED': True,
+        'DEFAULT_REQUEST_HEADERS': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Referer': 'https://www.pandaexpress.com/',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
         },
-        "USER_AGENT": None
-    }
-    headers = {
-        "host": "nomnom-prod-api.pandaexpress.com",
-        "clientid": "panda",
-        "ui-transformer": "restaurants",
-        "nomnom-platform": "web",
-        "accept": "application/json",
-        "content-type": "application/json",
-        "origin": "https://www.pandaexpress.com",
-        "referer": "https://www.pandaexpress.com/",
-        "accept-encoding": "gzip, deflate, br, zstd",
-        "accept-language": "en-US,en;q=0.9"
+        'CONCURRENT_REQUESTS': 64,
     }
 
+    def parse(self, response):
 
-    def start_requests(self) -> Iterable[Request]:
-        zipcodes = load_zipcode_data("data/zipcode_lat_long.json")
-        for zipcode in zipcodes:
-            url = f"https://nomnom-prod-api.pandaexpress.com/restaurants/near?lat={zipcode['latitude']}&long={zipcode['longitude']}&radius=20&limit=100&loyalty_filter=false&nomnom=calendars&nomnom_calendars_from=20241115&nomnom_calendars_to=20241124&nomnom_exclude_extref=99997,99996,99987,99988,99989,99994,11111,8888,99998,99999,0000"
-            yield scrapy.Request(url, callback=self.parse, headers=self.headers, meta={"impersonate": "chrome_android"})
+        if response.xpath('//div[@class="location_single__content"]'):
+            yield self.parse_store(response)
+        
+        all_loc_urls = response.xpath('//div[@class="locations_filter__content"]//div/a[@data-ga-action="storeDetailsClick" or @data-ga-action="locationClick"]/@href').getall()
+        for loc_url in all_loc_urls:
+            yield response.follow(loc_url, callback=self.parse)
 
+    def parse_store(self, response):
+        json_text = response.xpath('//script[@type="application/ld+json"]/text()').get()
+        data = json.loads(json_text)
+        return {
+            "name": response.xpath('//div[contains(@class, "name")]/h1/text()').get(),
+            "phone_number": data.get("telephone"),
+            "location": self._get_location(data),
+            "address": self._get_address(data.get("address", {})),
+            "hours": self._get_hours(data),
+            "url": response.url,
+            "raw": data
+        }
 
+    def _get_location(self, store_info) -> dict:
+        """Extract and format location coordinates."""
+        try:
+            latitude = store_info['geo']['latitude']
+            longitude = store_info['geo']['longitude']
 
-    def parse(self, response: Response):
-        restaurants = response.json()['restaurants']
-        for restaurant in restaurants:
-            yield {
-                "number": f"{restaurant['id']}",
-                "name": restaurant['name'],
-                "address": self._get_address(restaurant),
-                "location": {
+            if latitude is not None and longitude is not None:
+                return {
                     "type": "Point",
-                    "coordinates": [
-                        restaurant['longitude'],
-                        restaurant['latitude']
-                    ]
-                },
-                "hours": self._get_hours(restaurant),
-                "phone_number": restaurant['telephone'],
-                "raw": restaurant
-            }
-
-
-    def _get_address(self, restaurant: Dict) -> str:
+                    "coordinates": [float(longitude), float(latitude)]
+                }
+            self.logger.warning(f"Missing latitude or longitude for store")
+            return {}
+        except ValueError as e:
+            self.logger.warning(f"Invalid latitude or longitude values: {e}")
+        except Exception as e:
+            self.logger.error(f"Error extracting location: {e}", exc_info=True)
+        return {}
+    
+    def _get_address(self, address_info: dict[str, Any]) -> str:
+        """Format store address."""
         try:
             address_parts = [
-                restaurant.get("streetaddress", ""),
+                address_info.get("streetAddress", ""),
+                # address_info.get("address_2", ""),
             ]
             street = ", ".join(filter(None, address_parts))
 
-            city = restaurant.get("city", "")
-            state = restaurant.get("state", "")
-            zipcode = restaurant.get("zip", "")
-            if "-" in zipcode:
-                zipcode = zipcode.split("-")[0]
+            city = address_info.get("addressLocality", "")
+            state = address_info.get("addressRegion", "")
+            zipcode = address_info.get("postalCode", "")
 
             city_state_zip = f"{city}, {state} {zipcode}".strip()
 
-            return ", ".join(filter(None, [street, city_state_zip]))
+            full_address = ", ".join(filter(None, [street, city_state_zip]))
+            if not full_address:
+                self.logger.warning(
+                    f"Missing address information: {address_info}")
+            return full_address
         except Exception as e:
-            self.logger.error("Error getting address: %s", e, exc_info=True)
+            self.logger.error(f"Error formatting address: {e}", exc_info=True)
             return ""
 
-
-    def _get_hours(self, restaurant: Dict) -> Dict:
-        days_mapping = {
-            "sun": "sunday",
-            "mon": "monday",
-            "tue": "tuesday",
-            "wed": "wednesday",
-            "thu": "thursday",
-            "fri": "friday",
-            "sat": "saturday"
-        }
-        hours = {}
+    def _get_hours(self, store_info: dict[str, Any]) -> dict:
+        """Extract and format store hours."""
         try:
-            calendar = restaurant['calendars']['calendar']
-            if not calendar:
-                return {}
-            for _range in calendar[0]['ranges']:
-                day = days_mapping[_range['weekday'].lower()]
-                parsed_date_open = datetime.strptime(_range['start'], "%Y%m%d %H:%M")
-                parsed_date_close = datetime.strptime(_range['end'], "%Y%m%d %H:%M")
-                hours[day] = {
-                    "open": parsed_date_open.strftime("%I:%M %p").lower(),
-                    "close": parsed_date_close.strftime("%I:%M %p").lower()
-                }
-            return hours
+            hours_raw_list = store_info.get("openingHoursSpecification", [])
+            hours_dict = {}
+            for hours_obj in hours_raw_list:
+                day = hours_obj.get("dayOfWeek", "")
+                opens = hours_obj.get("opens", "").lower()
+                closes = hours_obj.get("closes", "").lower()
+                if day and opens and closes:
+                    hours_dict[day.lower()] = {
+                        "open": opens,
+                        "close": closes
+                    }
+            return hours_dict
         except Exception as e:
-            self.logger.error("Error getting hours: %s", e, exc_info=True)
+            self.logger.error(f"Error getting store hours: {e}", exc_info=True)
             return {}
